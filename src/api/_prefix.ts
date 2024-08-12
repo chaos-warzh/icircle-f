e324451a166c1a57b6c5d5f3096535f047c2882b
chaos-warzh <haidedeyu@163.com>
2024-08-12 23:57:56 +0800

    +prefix

---
+++ src/api/_prefix.ts
@@ -0,0 +1,10 @@
+// top api
+export const API_MODULE = '/api'
+
+export const USER_MODULE = `${API_MODULE}/users`
+
+export const CIRCLE_MODULE = `${API_MODULE}/circles`
+
+export const POST_MODULE = `${API_MODULE}/posts`
+
+export const COMMENTS_MODULE = `${API_MODULE}/comments`
